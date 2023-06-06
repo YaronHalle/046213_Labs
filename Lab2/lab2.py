ddff65892dd64398f12052c3c948dfe5d54d1d68
LahavBarak <lahavb94@gmail.com>
2023-06-06 18:18:51 +0300

    split point for lab2

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -28,7 +28,7 @@ def load_map_and_metadata(map_file):
             print(exc)
     origin_x = map_origin[0]
     origin_y = map_origin[1]
-        
+
     return map_arr, map_hight, map_width, map_resolution, origin_x, origin_y
 
 
@@ -37,10 +37,12 @@ def pose2map_coordinates(map_resolution, origin_x, origin_y, x, y):
     y_map = int((y - origin_y) / map_resolution)
     return y_map, x_map
 
+
 def map2pose_coordinates(map_resolution, origin_x, origin_y, x_map, y_map):
     x = x_map * map_resolution + origin_x
     y = y_map * map_resolution + origin_y
     return x, y
+
 
 def collision_check(map_arr, map_hight, map_width, map_resolution, origin_x, origin_y, x, y, theta):
     '''
@@ -49,21 +51,22 @@ def collision_check(map_arr, map_hight, map_width, map_resolution, origin_x, ori
     '''
     ####### your code goes here #######
     # TODO: transform configuration to workspace bounding box
-    
+
     # TODO: overlay workspace bounding box on map (creating borders for collision search in the next step)
-    
+
     # TODO: check for collisions by looking inside the bounding box on the map if there are values greater than 0
-    
+
     ##################################
 
     # Compute the robot's bounding box corners in respect to map coordinates.
     # We assume that 3 times the car's wheelbase bounds the robot's geometry in 2D.
-    wheelbase = 0.3302  # [meters
+    #### wheelbase = 0.3302  # [meters
+    wheelbase = 0  ## TODO find correct wheelbase for the actual relations between vehicle size and map resolution
     robot_bb_height = 3 * wheelbase  # [meters]
     robot_bb_width = 3 * wheelbase  # [meters]
 
     # Computing robot's bounding box coordinates
-    bb_ne_x = int(np.ceil(x + robot_bb_width/2))
+    bb_ne_x = int(np.ceil(x + robot_bb_width / 2))
     bb_ne_y = int(np.ceil(y + robot_bb_height / 2))
     bb_sw_x = int(np.floor(x - robot_bb_width / 2))
     bb_sw_y = int(np.floor(y - robot_bb_height / 2))
@@ -75,7 +78,9 @@ def collision_check(map_arr, map_hight, map_width, map_resolution, origin_x, ori
     # Overlay the robot's bounding box on map and check for obstacles existence
     return map_arr[bb_sw_row:bb_ne_row + 1, bb_sw_col:bb_ne_col + 1].any()
 
-def sample_configuration(map_arr, map_hight, map_width, map_resolution, origin_x, origin_y, n_points_to_sample=2000, dim=2):
+
+def sample_configuration(map_arr, map_hight, map_width, map_resolution, origin_x, origin_y, n_points_to_sample=2000,
+                         dim=2):
     # Extracting all FALSE indices locations (FALSE=free space)
     free_space_ind = np.argwhere(map_arr == False)
 
@@ -98,25 +103,31 @@ def sample_configuration(map_arr, map_hight, map_width, map_resolution, origin_x
 
     return samples
 
+
 def create_prm_traj(map_file):
     prm_traj = []
-    mid_points = np.array([[0,0,0],
-                           [9.5,4.5,np.pi/2],
-                           [0,8.5,np.pi],
-                           [-13.5,4.5,-np.pi/2]])
+    mid_points = np.array([[0, 0, 0],
+                           [9.5, 4.5, np.pi / 2],
+                           [0, 8.5, np.pi],
+                           [-13.5, 4.5, -np.pi / 2]])
     map_arr, map_hight, map_width, map_resolution, origin_x, origin_y = load_map_and_metadata(map_file)
 
     ####### your code goes here #######
     # TODO: load the map and metadata
     samples = sample_configuration(map_arr, map_hight, map_width, map_resolution, origin_x, origin_y)
+    test_x, test_y = map2pose_coordinates(map_resolution, origin_x, origin_y, 717, 1300)
+    is_colliding = np.array([collision_check(map_arr, map_hight, map_width, map_resolution,
+                                     origin_x, origin_y, sample[0], sample[1], 0) for sample in samples])
+    free_space_samples = np.argwhere(is_colliding == False)
+    samples = samples[free_space_samples, :]
     # TODO: create PRM graph
-    
+
     # TODO: create PRM trajectory (x,y) saving it to prm_traj list
-    
+
     ##################################
-    
+
     prm_traj = np.concatenate(prm_traj, axis=0)
-    np.save(os.path.join(pathlib.Path(__file__).parent.resolve().parent.resolve(),'resource/prm_traj.npy'), prm_traj)
+    np.save(os.path.join(pathlib.Path(__file__).parent.resolve().parent.resolve(), 'resource/prm_traj.npy'), prm_traj)
 
 
 def sample_control_inputs(number_of_samples=10):
@@ -126,10 +137,10 @@ def sample_control_inputs(number_of_samples=10):
     :return: Matrix number_of_samplesX2, first column is velocity and second column is the steering angle command
     '''
     # Predefined control cut-off limits
-    velocity_min = -0.5 # [m/s]
-    velocity_max = 0.5 # [m/s]
-    steering_angle_min = -0.3 # [rad]
-    steering_angle_max = 0.3 # [rad]
+    velocity_min = -0.5  # [m/s]
+    velocity_max = 0.5  # [m/s]
+    steering_angle_min = -0.3  # [rad]
+    steering_angle_max = 0.3  # [rad]
 
     # Drawing random samples.
     control_samples = np.random.rand(number_of_samples, 2)
@@ -139,6 +150,7 @@ def sample_control_inputs(number_of_samples=10):
     control_samples[:, 1] = steering_angle_min + (steering_angle_max - steering_angle_min) * control_samples[:, 1]
 
     return control_samples
+
 
 def forward_simulation_of_kineamtic_model(x, y, theta, v, delta, dt=0.5):
     '''
@@ -166,22 +178,24 @@ def forward_simulation_of_kineamtic_model(x, y, theta, v, delta, dt=0.5):
 
     return x_new, y_new, theta_new
 
+
 def create_kino_rrt_traj(map_file):
     kino_rrt_traj = []
-    mid_points = np.array([[0,0,0],
-                           [9.5,4.5,np.pi/2],
-                           [0,8.5,np.pi],
-                           [-13.5,4.5,-np.pi/2]])
+    mid_points = np.array([[0, 0, 0],
+                           [9.5, 4.5, np.pi / 2],
+                           [0, 8.5, np.pi],
+                           [-13.5, 4.5, -np.pi / 2]])
     map_arr, map_hight, map_width, map_resolution, origin_x, origin_y = load_map_and_metadata(map_file)
     ####### your code goes here #######
     # TODO: load the map and metadata
-    
+
     # TODO: create RRT graph and find the path saving it to kino_rrt_traj list
-    
+
     ##################################
-    
+
     kino_rrt_traj = np.array(kino_rrt_traj)
-    np.save(os.path.join(pathlib.Path(__file__).parent.resolve().parent.resolve(),'resource/kino_rrt_traj.npy'), kino_rrt_traj)
+    np.save(os.path.join(pathlib.Path(__file__).parent.resolve().parent.resolve(), 'resource/kino_rrt_traj.npy'),
+            kino_rrt_traj)
 
 
 if __name__ == "__main__":
